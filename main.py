afc1b4e0ae83f7b19b1a87d15614a6b941c8c004
williamsimms <williamsimms193@gmail.com>
2021-01-19 17:12:55 -0500

    fixes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 from requests import Response
 import hashlib
 
@@ -16,7 +17,9 @@ def request_api_data(query_char: str):
 def get_password_leaks_count(hashes: Response, hash_to_check):
     hashed_passwords = (line.split(':') for line in hashes.text.splitlines())
     for h, count in hashed_passwords:
-        print(h)
+        if h == hash_to_check:
+            return count
+    return 0
 
 
 def pwned_api_check(password: str):
@@ -25,3 +28,17 @@ def pwned_api_check(password: str):
     tail = sha1password[5:]
     response = request_api_data(first_five_chars)
     return get_password_leaks_count(response, tail)
+
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(
+                f'{password} was found {count} times.. you should change your password')
+        else:
+            print(f'{password} was not found.')
+    return 'done'
+
+
+main(sys.argv[1:])
