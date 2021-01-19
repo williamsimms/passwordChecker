ec91c350e1b357be906977a7746e4220cf28790d
williamsimms <williamsimms193@gmail.com>
2021-01-19 16:55:41 -0500

    added request api data

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import requests
+
+
+def request_api_data(query_char: str):
+    url = f'https://api.pwnedpasswords.com/{query_char}'
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise RuntimeError(
+            f'Error fetching:{response.status_code}, check the api and try again.')
+
+
+def pwned_api_check(password: str):
+    pass
